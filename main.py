f003fa8e2810c4d060993b0ea0a4211ca2074e40
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-09 17:24:03 +0100

    Switching working?

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def check_thread():
 # function for the placement of the boats faze
 def choose_faze():
     global current_player, boats_placed
+    temp = True
     #thread1 = threading.Thread(target=check_thread)
     #if thread1.is_alive() == False:
      #   thread1.start()
@@ -57,18 +58,24 @@ def choose_faze():
         for j in range(map_size):
             buttons[int(current_player)-1][i][j] = tkinter.Button(frame, text=" ", width=10, height=5, command=lambda i=i, j=j: place_boat(i, j))
             buttons[int(current_player)-1][i][j].grid(row=i, column=j)
-    while running:
+    while temp:
         if boats_placed == len(boat_types):
             boats_placed = 0
             if current_player == players[0]:
                 current_player = players[1]
+                print("player changed from 1 to 2")
+                temp = False
             else:
                 current_player = players[0]
-            main_label.config(text="Player " + current_player + "'s turn")
-            print("function ended")
+                print("player changed from 2 to 1")
+                temp = False
+            print("player changed")
         else:
             pass
-        print("function ended with a problem")
+        main_label.config(text="Player " + current_player + "'s turn")
+        #print("function ended")
+        window.update()
+    #print("function ended with a problem")
 
 
 # variables for the map, players, buttons, etc.
@@ -98,7 +105,7 @@ for i in range(num_of_players):
 
 # creating a thread for the mouse coordinates checking
 mouse_thread = threading.Thread(target=check_mouse_coordinates)
-chose_thread = threading.Thread(target=choose_faze)
+#chose_thread = threading.Thread(target=choose_faze)
 
 # creating the main window of the game
 window = tkinter.Tk()
@@ -128,9 +135,16 @@ button_frame.pack(side="bottom")
 
 
 for i in range(num_of_players):
-    chose_thread.start()
-    chose_thread.join()
-    print("thread ended")
+    #temp = current_player
+    choose_faze()
+    print("chose faze started")
+    #if current_player != temp:
+     #   choose_faze()
+    #print("chose faze started again")
+    #chose_thread.start()
+    #chose_thread.join()
+    #print("thread ended")
+
 # filling the boat selection frame with buttons representing the boats
 #for i in range(number_of_boats):
  #   boat_button = tkinter.Button(boat_frame, text=boat_types[i] + " (" + str(boat_sizes[i]) + ")", width=10, height=2, command=lambda i=i: select_boat(boat_types[i]))
@@ -151,6 +165,6 @@ settings_button.grid(row=0, column=1)
 
 
 
-
+print("window updated")
 #mouse_thread.join()
 window.mainloop()
